07f5181b1275eb13abfd9ac0054caaa5bb83d495
ghadeer-078 <90647463+ghadeer-078@users.noreply.github.com>
2021-11-26 03:08:17 +0300

    Menus App... Recreate your Button App to feature a menu
    You can display a simple snackbar message when menu items are tapped

--- app/src/main/java/com/example/buttonapp/MainActivity.kt
+++ app/src/main/java/com/example/buttonapp/MainActivity.kt
@@ -3,15 +3,21 @@ package com.example.buttonapp
 import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import android.view.View
 import android.widget.Button
 import android.widget.TextView
+import androidx.constraintlayout.widget.ConstraintLayout
+import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var numtxt: TextView
     private lateinit var add: Button
     private lateinit var sub: Button
+
+    private lateinit var clMain:ConstraintLayout
 
     var count = 0
 
@@ -22,9 +28,29 @@ class MainActivity : AppCompatActivity() {
         numtxt = findViewById(R.id.num)
         add = findViewById(R.id.button_plus)
         sub = findViewById(R.id.button_minus)
+        clMain = findViewById(R.id.clMain)
 
         add.setOnClickListener{ countHandler("+") }
         sub.setOnClickListener{ countHandler("-") }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            R.id.hi_message -> {
+                Snackbar.make(clMain, "Hello there!", Snackbar.LENGTH_SHORT).show()
+                return true
+            }
+            R.id.bye_message -> {
+                Snackbar.make(clMain, "Goodbye", Snackbar.LENGTH_SHORT).show()
+                return true
+            }
+        }
+        return super.onOptionsItemSelected(item)
     }
 
     private fun countHandler(op:String) {
@@ -49,4 +75,6 @@ class MainActivity : AppCompatActivity() {
         }
         numtxt.text = count.toString()
     }
+
+
 }
